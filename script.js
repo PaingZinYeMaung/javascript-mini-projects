60048d565324b1747cba3be4bd208914713dad1e
PaingZinYeMaung <paingzinyemaung.pgo@gmail.com>
2025-10-13 15:58:35 +0630

    fixed for bugs

--- script.js
+++ script.js
@@ -2,19 +2,20 @@
  document.addEventListener('click', function(e) {
     // console.log(e.target);
     // e.target.style.display = 'none';
-    e.target.style.backgroundColor = 'white';
-    e.target.textContent = "POP!";
-    popped++;
-    // console.log(popped);
-    checkAll();
-
+    if(e.target.className === 'balloon') {
+      e.target.style.backgroundColor = 'white';
+      e.target.textContent = "POP!";
+      popped++;
+      checkAll();
+    }
+    
  });
 
 function checkAll() {
      if(popped === 10) {
-        var balloonDiv = document.querySelectorAll('.balloons-div');
+        var balloonDiv = document.querySelector('.balloons-div');
         balloonDiv.innerHTML = '';
-        var noballoonDiv = document.querySelectorAll('#no-balloons');
-        noballoonDiv.style.display = 'block';
+        var noballoonDiv = document.querySelector('#no-balloons');
+        noballoonDiv.style.display = 'block';  
     }
 }
